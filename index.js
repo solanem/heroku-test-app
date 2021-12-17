0a7dd7ae2b153923c0f585a8132e2be499680f98
ViktorAttol <j.kolesnyk@gmx.de>
2021-12-17 15:34:42 +0100

    login possible

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const path = require('path');
 const app = express();
 const importData = require("./data.json");
 let port = process.env.PORT || 3000;
 
 app.get("/", (req, res)=> {
-    res.send("Hello World")
+    res.sendFile(path.join(__dirname,"index.html"));
 })
 
 app.get("/players", (req, res) =>{
