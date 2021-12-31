fe91ec6eea87aab5abb1cc37439ac1df84a9db36
solanem <92300514+solanem@users.noreply.github.com>
2021-12-31 07:01:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require('path');
 const app = express();
 
-let port = process.env.PORT || 3000;
+let port = process.env.PORT || 80;
 
 app.get("/", (req, res)=> {
     res.sendFile(path.join(__dirname,"index.html"));
